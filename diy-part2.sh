e7d356a5af88a428d2276ac04dd7d786130e878d
jack20231966 <126368178+jack20231966@users.noreply.github.com>
2023-05-08 10:38:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Openwrt '' package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /ShsirPLayGroud/g" package/lean/default-settings/files/zzz-default-settings
